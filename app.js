1f8841c0da80b705c9f5b9a4761c8a2c9fad7797
ShafaanAmir <124149395+ShafaanAmir@users.noreply.github.com>
2023-02-01 17:29:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ cross.addEventListener('click', function(){
 function downloadFile(){
     var link = document.createElement("a");
     link.download = "CV";
-    link.href = "../images/CV.png";
+    link.href = "../resume.png";
     link.click();
 }
 
